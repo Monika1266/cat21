f5dec273daf2d1a53aeed663686537079ff90c20
Monika1266 <monikasadhasivam766@gmail.com>
2024-05-31 09:22:19 +0530

    COMMITTED

---
+++ web1/src/main/java/prac1/web1/App.java
@@ -0,0 +1,13 @@
+package prac1.web1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
